ada646f9954e128fe3bf8f371dd45b22406fdfc1
mhweaver <mhweaver@gmail.com>
2026-07-20 07:56:51 -0600

    Auto-format panadapter FFT code

--- src/panadapter_fft.h
+++ src/panadapter_fft.h
@@ -11,38 +11,40 @@
 struct panadapter_fft;
 
 struct panadapter_fft_config {
-	int display_span_hz;
-	int center_hz;
-	int is_cw;
-	int is_tx;
-	int wpm;
-	int refresh_ms;
-	int display_width_px;
+  int display_span_hz;
+  int center_hz;
+  int is_cw;
+  int is_tx;
+  int wpm;
+  int refresh_ms;
+  int display_width_px;
 };
 
 struct panadapter_fft_frame {
-	int bins[PANADAPTER_FFT_FRAME_BINS];
-	int count;
-	double first_hz;
-	double bin_step_hz;
-	int decimation;
-	int observation_samples;
-	int fft_bins;
-	uint64_t sample_end_ms;
-	uint64_t generation;
-	struct panadapter_fft_config config;
+  int bins[PANADAPTER_FFT_FRAME_BINS];
+  int count;
+  double first_hz;
+  double bin_step_hz;
+  int decimation;
+  int observation_samples;
+  int fft_bins;
+  uint64_t sample_end_ms;
+  uint64_t generation;
+  struct panadapter_fft_config config;
 };
 
 struct panadapter_fft *panadapter_fft_create(void);
+
 void panadapter_fft_destroy(struct panadapter_fft *context);
-void panadapter_fft_push(struct panadapter_fft *context,
-	const double *i_samples, const double *q_samples, int count);
-void panadapter_fft_request(struct panadapter_fft *context,
-	const struct panadapter_fft_config *config);
-bool panadapter_fft_get_frame(struct panadapter_fft *context,
-	const struct panadapter_fft_config *config,
-	struct panadapter_fft_frame *frame);
+
+void panadapter_fft_push(struct panadapter_fft *context, const double *i_samples, const double *q_samples, int count);
+
+void panadapter_fft_request(struct panadapter_fft *context, const struct panadapter_fft_config *config);
+
+bool panadapter_fft_get_frame(struct panadapter_fft *context, const struct panadapter_fft_config *config, struct panadapter_fft_frame *frame);
+
 int panadapter_fft_frame_latency_ms(const struct panadapter_fft_frame *frame);
+
 void panadapter_fft_reset(struct panadapter_fft *context);
 
 #endif
